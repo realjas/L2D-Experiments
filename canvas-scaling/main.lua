839652f1f5f25240fcfcddf20cb55513cfc3d371
realjas <realjas@hotmial.com>
2018-03-05 17:02:49 -0500

    annotations and var name changes

--- canvas-scaling/main.lua
+++ canvas-scaling/main.lua
@@ -1,12 +1,13 @@
--- Image Buffer/RenderToTexture Experiment
+-- Scaling a low resolution game to dynamically fill the screen
 scale = 1
 centerCanvas = 0
 canvasWidth = 320
 canvasHeight = 240
 
 function love.load()
-	tiles = love.graphics.newImage("assets/images/screen.png")
+	image = love.graphics.newImage("assets/images/screen.png")
 
+    --Setup canvas with "nearest neighbor" as the scaling method
     canvas = love.graphics.newCanvas(cavasWidth, canvasHeight)
     canvas:setFilter("nearest", "nearest")
  
@@ -14,14 +15,14 @@ function love.load()
     love.graphics.setCanvas(canvas)
         love.graphics.clear()
         love.graphics.setBlendMode("alpha")
-        love.graphics.draw(tiles, 0, 0)
+        love.graphics.draw(image, 0, 0)
     love.graphics.setCanvas()
 
+    --scale and fill the screen using the contents of the canvas
     scale = math.floor(love.graphics.getHeight()/240)
     centerCanvas = ((love.graphics.getWidth()-(canvasWidth*scale))/2)
 end
  
 function love.draw()
-	--love.graphics.draw(tiles, 0, 0, 0, 10, 10)
     love.graphics.draw(canvas, centerCanvas, 0, 0, scale, scale)
 end
